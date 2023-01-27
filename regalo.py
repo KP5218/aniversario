a9ba35f909fb572f58d3ef7977754b46fb7698f2
KP <75393702+KP5218@users.noreply.github.com>
2023-01-27 17:50:20 -0300

    Add files via upload

---
+++ regalo.py
@@ -0,0 +1,40 @@
+from tkinter import messagebox
+messagebox.showinfo(message="Tu computador ha sido hackeado", title= "  \U0001F632"+"   Alerta")
+messagebox.showinfo(message="¿No sabes que sucede?", title="  \U0001F632"+"   Alerta")
+messagebox.showinfo(message="Es que hoy se cumplen 5 años!!!!", title="Feliz Aniversario!!!!")
+pregunta1 = (messagebox.askyesno("Feliz Aniversario!!!!","¿El tiempo pasa muy rapido no te parece?"))
+if pregunta1 == True:
+   messagebox.showinfo("\U0001F499 Alerta	","Pues a mi tambien")
+   messagebox.showinfo("\U0001F499 Alerta	","Y tengo una pregunta para ti")
+   Mensaje1=(messagebox.askyesno("\U0001F914  Alerta","¿Quieres seguir compartiendo tu vida conmigo?"))
+   if Mensaje1 == False:
+    messagebox.showinfo("Desgraciada","No???")
+    messagebox.showinfo("Desgraciada","Pues JODETE!!")
+    messagebox.showinfo("Desgraciada","ya no te quiero")
+    messagebox.showinfo("Desgraciada","uuuuuuuyyyy ")
+    messagebox.showinfo("Desgraciada","\U0001F595 \U0001F595 \U0001F595 \U0001F595")
+   if Mensaje1 == True:
+    messagebox.showinfo("\U0001F648 Alerta","Gracias por decir sí")
+    messagebox.showinfo("\U0001F648 Alerta","No imaginas lo que siento por ti")
+    messagebox.showinfo("\U0001F648 Alerta","Solo te puedo decir que")
+    messagebox.showinfo("\U0001F648 Alerta","Te amo cada día mas y quiero que no lo olvides nunca")
+    messagebox.showinfo("\U0001F648 Alerta","Y en este día, te deseo la maxima felicidad")
+    messagebox.showinfo("\U0001F648 Alerta","Feliz aniversario 5!!")
+    
+if pregunta1 == False:
+    messagebox.showinfo("\U0001F92C alerta","Como que no??")
+    messagebox.showinfo("\U0001F92C alerta","Pues yo digo que si es increible")
+    messagebox.showinfo("\U0001F92C alerta","Y punto")
+    messagebox.showinfo("\U0001F92C alerta","punto")
+    messagebox.showinfo("\U0001F92C alerta","punto")
+    messagebox.showinfo("\U0001F92C alerta","y yo gane")
+    Pregunta2 = (messagebox.askyesno("\U0001F62C continuemos","Bueno, deseas seguir con el regalo??"))
+    if Pregunta2 == False:
+        messagebox.showinfo(" \U0001F595 Alerta","Porque no??")
+        messagebox.showinfo("\U0001F595 Alerta","uuy me caes mal!!!")
+        messagebox.showinfo("\U0001F595 Alerta","muy mal")
+        messagebox.showinfo("\U0001F595 Alerta","Pesimo")
+        messagebox.showinfo("\U0001F595 Alerta","anda a comer caca de perro, a la punta del cerro")
+    if Pregunta2 == True:
+        messagebox.showinfo("\U0001F644 Alerta","Ya se me olvido que te iba a decir")
+        messagebox.showinfo("\U0001F644 Alerta","Adios")
